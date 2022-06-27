6e57882adfb5efd2d56bf55c0ffe51d30cbcff6d
jcho19 <josephcho212@gmail.com>
2022-06-26 21:52:29 -0700

    added viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import Category, Api
+from django.contrib.auth.models import User
+from .serializers import CategorySerializer, ApiSerializer, UserSerializer
+from rest_framework import permissions, viewsets
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ApiViewSet(viewsets.ModelViewSet):
+    queryset = Api.objects.all()
+    serializer_class = ApiSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
