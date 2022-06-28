6ba002251d1ba7abb8f488414796416074e2e89b
jcho19 <josephcho212@gmail.com>
2022-06-28 08:47:48 -0700

    revised router #2

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import SimpleRouter
 from .views import CategoryViewSet, ApiViewSet, UserViewSet
-router = SimpleRouter(trailing_slash=False)
+router = SimpleRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'apis', ApiViewSet, basename='apis')
 router.register(r'users', UserViewSet, basename='users')
