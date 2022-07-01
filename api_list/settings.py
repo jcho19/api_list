2a2a09db6688a86153d4c9c53df33adb48ad3148
jcho19 <josephcho212@gmail.com>
2022-07-01 14:24:47 -0700

    revised metadata #2

--- api_list/settings.py
+++ api_list/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'API List REST API',
-    'DESCRIPTION': 'Stores categories and information of public APIS',
+    'DESCRIPTION': 'Stores categories and information on public APIS',
     'VERSION': '1.0.0',
     
 }
